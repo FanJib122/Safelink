17ea4aa2912a3b7fb5478fe822decc05245c8b59
FanJib122 <30363827+FanJib122@users.noreply.github.com>
2017-07-25 19:58:05 +0700

    Update multi-link2.js

--- multi-link2.js
+++ multi-link2.js
@@ -23,7 +23,7 @@ if(x=="1"){xxx="/2017/07/auto-insurance-quotes-2015.html"}
 if(x=="2"){xxx="/2017/07/ways-to-get-cheapest-health-insuranc.html}
 if(x=="3"){xxx="/2017/07/google-drive-cloud.html"}
 if(x=="4"){xxx="/2017/07/stress-eating-helps.html"}
-if(x=="5"){xxx="/2017/07/how-to-read-forex-chart-properly.html"}a_to_ve[i].href="http://converter-link.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="/2017/07/how-to-read-forex-chart-properly.html"}a_to_ve[i].href="https://caranyakamisafelink.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
